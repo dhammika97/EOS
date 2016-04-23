a4416cecc218f12f54cdd61980a14cea3d529cca
deegha <shuboothi@gmail.com>
2016-04-23 21:46:19 +0530

    created file upload

--- api/include/upload.php
+++ api/include/upload.php
@@ -1,26 +1,40 @@
 <?php
-require_once 'SimpleImage.php';
 if ( !empty( $_FILES ) ) {
 	
-	if(isset($_FILES['news_image']['name']) != ""){
-		$dir = '../uploads/news/';
-		$unique = strtoupper(md5(uniqid(rand(), true)));	
-		$image = new SimpleImage();
-		$image->load($_FILES['news_image']['tmp_name']);
-		$image->resizeToWidth(466);
-		$image->save($dir.$unique.$_FILES['news_image']['name']);
-		$response = array('error' => false, 'message'=>'File transfered completed!','image' =>$unique.$_FILES['news_image']['name']);
-		$json = json_encode($response);
-		echo $json;
-	}else{
-		$response = array('error' => true, 'message'=>'Bad Request');
-		$json = json_encode($response);
-		echo $json;	
+	$target_dir = "uploads/";
+	$unique = strtoupper(md5(uniqid(rand(), true)));
+	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]).$unique;
+	$uploadOk = 1;
+	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	
+
+	if($imageFileType != "csv") {
+	    
+	    $uploadOk = 0;
 	}
+
+	if ($uploadOk == 0) {
+	   $response = array('error' => True, 'message' => 'File type not support');
+	    $json = json_encode($response);
+	    echo $json;
+	} else {
+	    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+	       	$response = array('error' => True, 'message' => 'File successfully uploaded', 'file_name' => $target_file);
+		    $json = json_encode($response);
+		    echo $json;
+	    } else {
+	    	$response = array('error' => True, 'message' => 'File upload faild', 'file_name' => '');
+		    $json = json_encode($response);
+		    echo $json;
+	         
+	    }
+	}
+
+
 } else {
-
-    echo 'No files';
-
+	$response = array('error' => True, 'message' => 'File type not support');
+    $json = json_encode($response);
+    echo $json;
 }
 
 ?>
